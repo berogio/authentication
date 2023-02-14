e72573d56f77e3e01c87b9671b752b2665ea423e
berogio <giorgi.beridze01e@geolab.edu.ge>
2023-02-14 21:55:32 +0100

    AddedComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
