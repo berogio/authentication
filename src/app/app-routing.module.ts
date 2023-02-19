42beaf09fb758156eb16993039b1942ee3b84634
berogio <giorgi.beridze01e@geolab.edu.ge>
2023-02-19 15:11:25 +0100

    addedrouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PanelComponent } from './panel/panel.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'panel',
     component: PanelComponent,
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent,
   },
 ];
 
